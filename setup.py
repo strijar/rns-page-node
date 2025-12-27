fc50bc6fb5a102f4fbaed87c18919029dc30f67d
Sudo-Ivan <ivan@quad4.io>
2025-12-27 12:58:18 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rns-page-node",
+    version="1.3.0",
+    description="A simple way to serve pages and files over the Reticulum network.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sudo-Ivan",
+    url="https://git.quad4.io/RNS-Things/rns-page-node",
+    packages=find_packages(),
+    install_requires=[
+        "rns>=1.0.4,<1.5.0",
+        "cryptography>=46.0.3",
+    ],
+    entry_points={
+        "console_scripts": [
+            "rns-page-node=rns_page_node.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires=">=3.9.2",
+)
+
