37bc4948d136c899b119d15b0d3ea8413d957125
Sudo-Ivan <ivan@quad4.io>
2025-12-28 22:58:35 -0600

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A simple way to serve pages and files over the Reticulum network";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        python = pkgs.python3;
+
+        pythonPackages = python.pkgs;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python
+            pythonPackages.poetry
+            pythonPackages.build
+            pythonPackages.pip
+            pythonPackages.setuptools
+            pythonPackages.wheel
+            pythonPackages.ruff
+          ];
+        };
+      });
+}
+
