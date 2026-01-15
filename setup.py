b1de58da7256f64560b8e2c63415359a77438b41
Sudo-Ivan <ivan@quad4.io>
2026-01-15 13:53:59 -0600

    Update version to 1.4.0 in pyproject.toml and setup.py, and update content-hash in poetry.lock

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="rns-page-node",
-    version="1.3.1",
+    version="1.4.0",
     description="A simple way to serve pages and files over the Reticulum network.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
